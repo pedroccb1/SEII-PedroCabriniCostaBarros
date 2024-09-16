a36f8b5b0c55a43f46f1c02bde1237bded7ffbcd
pedroccb1 <pedro.cabrini2002@gmail.com>
2024-09-15 23:58:46 -0300

    Add files via upload

---
+++ Semana05/Exercicio03/Ex03/BGandFGThreads/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BGandFGThreads
+{
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+        for (int i = 0; i < Environment.ProcessorCount; i++)
+        {
+            var t1 = new Thread(DifferentMethod);
+            t1.IsBackground = true;
+            t1.Start(i);
+        }
+        Console.WriteLine("Leaving Main");
+    }
+    static void DifferentMethod(object threadID)
+    {
+        while(true)
+            Console.WriteLine("Hello from DifferentMethod (" + Thread.CurrentThread.ManagedThreadId + ")");
+    }
+}
+}
---
+++ Semana05/Exercicio03/Ex05/BasicThreadSynchronization/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BGandFGThreads
+{
+    class Program
+    {
+        static int count = 0;
+        static object baton = new object();
+        static void Main(string[] args)
+        { 
+            var t1 = new Thread(IncrementCount);
+            var t2 = new Thread(IncrementCount);
+            t1.Start();
+            Thread.Sleep(500);
+            t2.Start();
+        }
+        static void IncrementCount()
+        {
+            while (true)
+            {
+                lock(baton)
+                {
+                    int temp = count;
+                    Thread.Sleep(1000);
+                    count = temp +1;
+                    Console.WriteLine("Thread ID " + Thread.CurrentThread.ManagedThreadId + 
+                        " incremented count to " + count + ".");
+                }
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
+
