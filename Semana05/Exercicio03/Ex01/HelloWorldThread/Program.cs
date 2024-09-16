bdd3a3a792deb15870c003fc2ecfd029fc2b48db
pedroccb1 <pedro.cabrini2002@gmail.com>
2024-09-15 23:49:02 -0300

    Add files via upload

---
+++ Semana05/Exercicio03/Ex01/HelloWorldThread/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HelloWorldThread
+{
+class Program
+{
+    static void Main(string[] args)
+    {
+        var t1 = new Thread(DifferentMethod);
+        t1.Start();
+    }
+    static void DifferentMethod()
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
+}
---
+++ Semana05/Exercicio03/Ex02/MultipleThreads/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultipleThreads
+{
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            var t1 = new Thread(DifferentMethod);
+            t1.Start(i);
+        }
+        // while(true)
+        //     Console.WriteLine("Hello from Main (1)");
+    }
+    static void DifferentMethod(object threadID)
+    {
+        while(true)
+            Console.WriteLine("Hello from DifferentMethod (" + threadID + ")");
+    }
+}
+}
