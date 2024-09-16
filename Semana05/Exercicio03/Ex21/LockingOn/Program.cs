b7e3d61c1ac2ddccd614297d4765c4d8517e7be6
pedroccb1 <pedro.cabrini2002@gmail.com>
2024-09-16 00:12:41 -0300

    Add files via upload

---
+++ Semana05/Exercicio03/Ex21/LockingOn/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Numerics;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace LockingOn
+{
+    class BathroomStall
+    {
+        object baton = new object();
+        public void BeUsed()
+        {
+            lock(baton)
+                Console.WriteLine("Doing our thing...");
+        }
+        public void FlushToilet()
+        {
+            lock(baton)
+                Console.WriteLine("Flushing the toilet...");
+        }
+    }
+    class PublicRestroom
+    {
+        BathroomStall stall1 = new BathroomStall();
+        BathroomStall stall2 = new BathroomStall();
+        public void UseStall1()
+        {
+            stall1.BeUsed();
+            stall1.FlushToilet();
+        }
+        public void UseStall2()
+        {
+            stall2.BeUsed();
+            stall2.FlushToilet();
+        }
+        public void CleanTheSink()
+        {
+            lock (stall1)
+                Console.WriteLine("Clean the sink...");
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var restroom = new PublicRestroom();
+            new Thread(restroom.UseStall1).Start();
+            new Thread(restroom.UseStall2).Start();
+            new Thread(restroom.UseStall1).Start();
+            new Thread(restroom.UseStall2).Start();
+        }
+    }
+}
---
+++ Semana05/Exercicio03/Ex22/LockingOnVsMethod/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace LockingOnVsMethod
+{
+    class BathroomStall
+    {
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        public static void BeUsed()
+        {
+            lock (typeof(BathroomStall))
+            {
+                Console.WriteLine("Doing our thing...");
+                Thread.Sleep(5000);
+            }
+        }
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        public static void FlushToilet()
+        {
+            Console.WriteLine("Flushing the toilet...");
+            Thread.Sleep(5000);
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Thread(BathroomStall.BeUsed).Start();
+            new Thread(BathroomStall.FlushToilet).Start();
+        }
+    }
+}
