83014e6404b59c9ea7f550fb2790b57a07ce6342
pedroccb1 <pedro.cabrini2002@gmail.com>
2024-09-16 00:02:41 -0300

    Add files via upload

---
+++ Semana05/Exercicio03/Ex11/ProducerConsumerThread/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Numerics;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace ProducerConsumerThread
+{
+    class Program
+    {
+        static byte[] values = new byte[500000000];
+        static long[] portionResults;
+        static int portionSize;
+        static int numThreads;
+
+        static void GenerateInts()
+        {
+            var rand = new Random(987);
+            for (int i = 0; i < values.Length; i++)
+                values[i] = (byte)rand.Next(10);
+        }
+        static void SumYourPortion(object portionNumber)
+        {
+            long sum = 0;
+            int portionNumberAsInt = (int)portionNumber;
+            int baseIndex =  portionNumberAsInt * portionSize;
+            for (int i = baseIndex; i < baseIndex + portionSize; i++)
+                sum += values[i];
+            portionResults [portionNumberAsInt] = sum;
+        }
+        static void Main(string[] args)
+        {
+            portionResults = new long[Environment.ProcessorCount];
+            portionSize = values.Length / Environment.ProcessorCount;
+            GenerateInts();
+            Console.WriteLine("Summing...");
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            long total1 = 0;
+            for(int i = 0; i < values.Length; i++)
+                total1 += values[i];
+            watch.Stop();
+            Console.WriteLine("Total values is: " + total1);
+            Console.WriteLine("Time to sum: " + watch.Elapsed);
+            Console.WriteLine();
+
+            watch.Reset();
+            watch.Start();
+            Thread[] threads =  new Thread[Environment.ProcessorCount];
+            for (int i = 0; i < Environment.ProcessorCount; i++)
+            {
+                threads[i] = new Thread(SumYourPortion);
+                threads[i].Start(i);
+            }
+            for (int i = 0; i < Environment.ProcessorCount; i++)
+                threads[i].Join();
+            long total2 = 0;
+            for (int i = 0; i < Environment.ProcessorCount; i++)
+                total2 += portionResults[i];
+            watch.Stop();
+            Console.WriteLine("Total values is: " + total2);
+            Console.WriteLine("Time to sum: " + watch.Elapsed);
+        }
+    }
+}
---
+++ Semana05/Exercicio03/Ex12/ProducerConsumerThread/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Numerics;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace ProducerConsumerThread
+{
+    class Program
+    {
+        static Queue<int> numbers = new Queue<int>();
+        static Random rand = new Random();
+        const int NumThreads = 3;
+        static int[] sums = new int[NumThreads];
+        static void ProduceNumbers()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                numbers.Enqueue(rand.Next(10));
+                Thread.Sleep(rand.Next(1000));
+            }
+        }
+        static void SumNumbers(object threadNumber)
+        {
+            DateTime startTime = DateTime.Now;
+            int mySum = 0;
+            while((DateTime.Now - startTime).Seconds < 11)
+            {
+                if (numbers.Count != 0)
+                {
+                    mySum += numbers.Dequeue();
+                }
+            }
+            sums[(int)threadNumber] = mySum;
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
